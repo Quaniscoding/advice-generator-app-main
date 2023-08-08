714a2250d8455c34f58462c98c0dc0af89c54881
Quaniscoding <vq.2509.2003@gmail.com>
2023-08-08 22:04:48 +0700

    add audio onClick

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttons = document.querySelectorAll("button");
 document.getElementById('btnGetRandom').onclick = async function () {
+    audio.play();
     const response = await fetch("https://api.adviceslip.com/advice");
     const getAdvice = await response.json();
     let id = getAdvice.slip.id;
